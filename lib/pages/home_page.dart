a8fbb15deb9a9ced9970ad3662a0cefb280c8464
DaroHang <56570369+darohang088@users.noreply.github.com>
2025-11-24 21:47:02 +0700

    update yml

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:weather_app/utils/app_safe_area.dart';
 import 'package:weather_app/utils/spacing_extension.dart';
 import 'package:weather_app/widget.dart/forcecaset_switcher.dart';
 import 'package:weather_app/widget.dart/weatheer_card.dart';
 import 'package:weather_app/widget.dart/weather_alert_section.dart';
 import 'package:weather_app/widget.dart/weather_metrics_card.dart';
-import '../../providers/user_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,8 +16,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final home = context.watch<HomeProvider>();
-
     return Scaffold(
       body: AppSafeArea(
         padding: 20.paddingAll,
